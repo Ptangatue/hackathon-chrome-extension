85fbcb267813d57f97516c779635cd681c5a4771
Kevin Wu <wu.haoran2002@gmail.com>
2025-02-06 22:40:05 -0500

    added floating text box. need to work on grabbing values

---
+++ content.js
@@ -0,0 +1,18 @@
+// document.addEventListener('DOMContentLoaded', () => {
+const text = document.createElement('p');
+text.innerText = 'Array: \n For Loop:';
+text.style.color = 'white';
+text.style.position = 'fixed';
+text.style.top = '100px';
+text.style.right = '20px';
+text.style.backgroundColor = 'black';
+text.style.padding = '10px';
+text.style.zIndex = '1000'; // Ensure it stays on top
+
+document.querySelector('body').appendChild(text);
+
+// const value = document.querySelector(
+//   '.unit-progress text-right col-xs-12 p'
+// ).innerText;
+// console.log(value);
+// });
